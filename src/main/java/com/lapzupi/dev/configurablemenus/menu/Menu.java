92922278e87f25a4ae5ea9ad1fd388334905ec21
Omer Oreg <sarhatabaot1@gmail.com>
2022-07-30 14:45:54 +0300

    Added basic menu structure

---
+++ src/main/java/com/lapzupi/dev/configurablemenus/menu/Menu.java
@@ -0,0 +1,21 @@
+package com.lapzupi.dev.configurablemenus.menu;
+
+import java.util.List;
+
+/**
+ * @author sarhatabaot
+ */
+public class Menu {
+    private final String id;
+    private final String permission;
+    private String title;
+    private final MenuType type;
+    private int rows;
+    private List<MenuItem> items;
+    private List<MenuItem> fillers;
+    /*
+    default action
+    open action
+    close action
+     */
+}
