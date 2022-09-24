ade8a87a5a8b7d1f2fc5c57048a8e0a52320bc34
Omer Oreg <sarhatabaot1@gmail.com>
2022-09-24 09:41:59 +0300

    Added getUrl method and improved docs

--- src/main/java/com/lapzupi/dev/configurablemenus/addons/ItemAddon.java
+++ src/main/java/com/lapzupi/dev/configurablemenus/addons/ItemAddon.java
@@ -7,22 +7,45 @@ import org.bukkit.inventory.ItemStack;
  * @author sarhatabaot
  */
 public abstract class ItemAddon{
+    /**
+     * @param id id of the ItemStack
+     * @return The ItemStack via the id
+     */
     public abstract ItemStack getItemStack(final String id);
 
     /**
-     * @return The prefix used by the addon: "playerhead:id" or "base64:id"
+     * @return The prefix used by the addon: "playerhead:id" or "base64:id in `prefix:id`"
      */
     public abstract String getPrefix();
 
+    /**
+     * @return The dependant plugin name, if there isn't one, you can set this to null.
+     * You should override {@link #canRegister() canRegister() method} if there is no dependant plugin.
+     */
     public abstract String getPluginName();
 
+
+    /**
+     * Can this addon be registered.
+     */
     public boolean canRegister() {
         return (Bukkit.getPluginManager().getPlugin(getPluginName()) != null);
     }
 
+    /**
+     * @return The author's name
+     */
     public abstract String getAuthor();
 
+    /**
+     * @return The addon's version
+     */
     public abstract String getVersion();
+
+    /**
+     * @return Return a link to the addons project page or download link
+     */
+    public abstract String getUrl();
 
     @Override
     public final String toString() {
