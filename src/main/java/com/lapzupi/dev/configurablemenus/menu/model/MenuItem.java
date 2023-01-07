4e1e049963f6ce7b838178ce7628edb71e655872
Omer Oreg <sarhatabaot1@gmail.com>
2023-01-07 21:10:48 +0200

    Fixes ActionAddons not working

--- src/main/java/com/lapzupi/dev/configurablemenus/menu/model/MenuItem.java
+++ src/main/java/com/lapzupi/dev/configurablemenus/menu/model/MenuItem.java
@@ -1,13 +1,11 @@
 package com.lapzupi.dev.configurablemenus.menu.model;
 
-import com.github.sarhatabaot.kraken.core.chat.ChatUtil;
 import com.lapzupi.dev.configurablemenus.ConfigurableMenusPlugin;
 import com.lapzupi.dev.configurablemenus.addons.ActionAddon;
 import dev.triumphteam.gui.builder.item.ItemBuilder;
 import dev.triumphteam.gui.components.GuiAction;
 import dev.triumphteam.gui.guis.GuiItem;
 import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -28,7 +26,7 @@ public class MenuItem {
     private List<String> onLeftClick;
     private List<String> onShiftClick;
     private List<String> onRightClick;
-
+    
     public MenuItem(final int row, final int column, final ItemSettings settings, final List<Duplicate> duplicate, final List<String> onLeftClick, final List<String> onShiftClick, final List<String> onRightClick) {
         this.row = row;
         this.column = column;
@@ -38,7 +36,7 @@ public class MenuItem {
         this.onShiftClick = onShiftClick;
         this.onRightClick = onRightClick;
     }
-
+    
     public GuiItem getAsGuiItem() {
         ItemStack baseItemStack;
         try {
@@ -47,30 +45,30 @@ public class MenuItem {
             //log
             baseItemStack = new ItemStack(Material.AIR);
         }
-
+        
         ItemBuilder builder = ItemBuilder.from(baseItemStack)
-                .amount(settings.getAmount());
-
+            .amount(settings.getAmount());
+        
         if (!settings.getDisplayName().isEmpty()) {
             builder.name(Component.text(settings.getDisplayName()));
         }
-
+        
         if (settings.getCustomModelData() != -1) {
             builder.model(settings.getCustomModelData());
         }
-
+        
         GuiItem guiItem = builder.asGuiItem();
-
+        
         if (!onLeftClick.isEmpty() || !onShiftClick.isEmpty() || !onRightClick.isEmpty()) {
             guiItem.setAction(new ClickAction());
-
+            
         }
         return guiItem;
     }
-
+    
     public class ClickAction implements GuiAction<InventoryClickEvent> {
         private final ConfigurableMenusPlugin plugin = (ConfigurableMenusPlugin) JavaPlugin.getProvidingPlugin(ConfigurableMenusPlugin.class);
-
+        
         @Override
         public void execute(final @NotNull InventoryClickEvent event) {
             if (!(event.getWhoClicked() instanceof Player player)) {
@@ -81,88 +79,65 @@ public class MenuItem {
                 event.setCancelled(true);
                 return;
             }
-
+            
             if (event.getClick().isRightClick()) {
                 onClick(player, onRightClick);
                 event.setCancelled(true);
                 return;
             }
-
+            
             if (event.getClick().isShiftClick()) {
                 onClick(player, onShiftClick);
                 event.setCancelled(true);
             }
         }
-
+        
         //abstract this as well at some point, enabling people to add their own actions if they want.
         private void onClick(final Player player, final List<String> actions) {
             for (String string : actions) {
-                final String prefix = string.split(":")[0];
-                final String args = string.split(":")[1];
+                final String prefix = string.split(":", 2)[0];
+                final String args = string.split(":", 2)[1];
                 ActionAddon actionAddon = this.plugin.getAddonManager().getActionAddon(prefix);
-                if(actionAddon != null) {
-                    actionAddon.onClick(player,args);
+                if (actionAddon != null) {
+                    actionAddon.onClick(player, args);
                 }
-//                if (string.startsWith("command:")) {
-//                    runCommand(player, string.split(":")[1]);
-//                } else if (string.startsWith("open-link:")) {
-//                    openLink(player, string.split(":")[1]);
-//                } else if (string.startsWith("message:")) {
-//                    message(player, string.split(":")[1]);
-//                }
             }
         }
-
-        @Deprecated(since = "0.2.1", forRemoval = true)
-        private void runCommand(final Player player, final String command) {
-            final String formattedCommand = ChatUtil.format(player,command);
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), formattedCommand);
-        }
-
-        @Deprecated(since = "0.2.1", forRemoval = true)
-        private void openLink(final Player player, final String link) {
-            message(player,link);
-        }
-
-        @Deprecated(since = "0.2.1", forRemoval = true)
-        private void message(final Player player, final String message) {
-            ChatUtil.sendMessage(player, ChatUtil.format(player,message));
-        }
     }
-
-
+    
+    
     public static class InvalidMaterialException extends Exception {
         public InvalidMaterialException(final String message) {
             super(message);
         }
     }
-
+    
     public int getRow() {
         return row;
     }
-
+    
     public int getColumn() {
         return column;
     }
-
+    
     public ItemSettings getSettings() {
         return settings;
     }
-
+    
     public List<Duplicate> getDuplicate() {
         return duplicate;
     }
-
+    
     @Override
     public String toString() {
         return "MenuItem{" +
-                "row=" + row +
-                ", column=" + column +
-                ", settings=" + settings +
-                ", duplicate=" + duplicate +
-                ", onLeftClick=" + onLeftClick +
-                ", onShiftClick=" + onShiftClick +
-                ", onRightClick=" + onRightClick +
-                '}';
+            "row=" + row +
+            ", column=" + column +
+            ", settings=" + settings +
+            ", duplicate=" + duplicate +
+            ", onLeftClick=" + onLeftClick +
+            ", onShiftClick=" + onShiftClick +
+            ", onRightClick=" + onRightClick +
+            '}';
     }
 }
