8cc3d07f20bf1ff23a49928e197a7ba874a2cb0f
Omer Oreg <sarhatabaot1@gmail.com>
2023-04-12 09:53:07 +0300

    Update adventure & platform-bukkit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,10 +13,10 @@ dependencyResolutionManagement {
             library("paper-api", "io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
             library("vault-api", "com.github.MilkBowl:VaultAPI:1.7.1")
             
-            version("adventure", "4.13.0")
+            version("adventure", "4.13.1")
             library("adventure-api", "net.kyori","adventure-api").versionRef("adventure")
             library("adventure-minimessage", "net.kyori", "adventure-text-minimessage").versionRef("adventure")
-            library("adventure-bukkit", "net.kyori:adventure-platform-bukkit:4.2.0")
+            library("adventure-bukkit", "net.kyori:adventure-platform-bukkit:4.3.0")
             
             library("placeholder-api", "me.clip:placeholderapi:2.11.3")
             
